b10194de129268b44126653ff44ecf3e7f51f427
u1809 <1473925065@qq.com>
2019-04-23 10:55:13 +0000

    jssdk

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -20,16 +20,16 @@
 <script>
     wx.config({
         //debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: "{{$jsconfig['appId']}}", // 必填，公众号的唯一标识
-        timestamp: "{{$jsconfig['timestamp']}}", // 必填，生成签名的时间戳
-        nonceStr: "{{$jsconfig['nonceStr']}}", // 必填，生成签名的随机串
-        signature: "{{$jsconfig['signature']}}",// 必填，签名
+        appId: "{{$js_info['appId']}}", // 必填，公众号的唯一标识
+        timestamp: "{{$js_info['timestamp']}}", // 必填，生成签名的时间戳
+        nonceStr: "{{$js_info['nonceStr']}}", // 必填，生成签名的随机串
+        signature: "{{$js_info['signature']}}",// 必填，签名
         jsApiList: ['chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
     });
     wx.ready(function(){
         $("#btn1").click(function(){
             wx.chooseImage({
-                count: 2, // 默认9
+                count: 2, // 默认可以选9个
                 sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                 success: function ( res) {
